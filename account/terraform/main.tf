13dcefd17efa62ec5a98a7ecd197775275663be9
Aeden Jameson <aeden.jameson@gmail.com>
2024-12-04 13:52:18 -0800

    remove unused utils module

--- account/terraform/main.tf
+++ account/terraform/main.tf
@@ -8,12 +8,3 @@ data "aws_availability_zones" "available" {
     values = ["opt-in-not-required"]
   }
 }
-
-module "utils" {
-  source  = "cloudposse/utils/aws"
-  version     = "1.4.0"
-}
-
-locals {
-  code_maps = module.utils.region_az_alt_code_maps
-}
