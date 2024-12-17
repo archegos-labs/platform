0761f376e2cb883d7f1dc9791d1c00a0957f921c
Aeden Jameson <aeden.jameson@gmail.com>
2024-12-17 11:10:21 -0800

    add kubeflow issuer

--- kubeflow/terraform/main.tf
+++ kubeflow/terraform/main.tf
@@ -8,3 +8,18 @@ resource "kubernetes_namespace" "kubeflow" {
     name = "kubeflow"
   }
 }
+
+module "kubeflow_issuer" {
+  source  = "aws-ia/eks-blueprints-addon/aws"
+  version = "1.1.1"
+
+  name             = "kubeflow-issuer"
+  description      = "A Helm chart to deploy kubeflow-issuer"
+  chart            = "../charts/kubeflow-issuer"
+  chart_version    = "v1.6.1"
+
+  wait                       = true
+  wait_for_jobs              = true
+
+  depends_on    = [kubernetes_namespace.kubeflow]
+}
