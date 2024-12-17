9933091677e8d332804ade2e203104ac2c1a2e31
Aeden Jameson <aeden.jameson@gmail.com>
2024-12-16 21:24:26 -0800

    correct addon name

--- eks/addons/nvidia-gpu-operator/terraform/main.tf
+++ eks/addons/nvidia-gpu-operator/terraform/main.tf
@@ -1,17 +1,17 @@
 locals {
   namespace = "gpu-operator"
-  addon_name = "nvidia-gpu-operator"
+  addon_name = "gpu-operator"
 }
 
 /**
   Code: https://github.com/NVIDIA/gpu-operator
   Docs: https://docs.nvidia.com/datacenter/cloud-native/gpu-operator/latest/amazon-eks.html
 */
-module "nvidia-gpu-operator" {
+module "nvidia_gpu_operator" {
   source  = "aws-ia/eks-blueprints-addon/aws"
   version = "1.1.1"
 
-  name             = local.addon_name
+  name             = "nvidia-${local.addon_name}"
   description      = "A Helm chart to deploy nvidia-gpu-operator"
   namespace        = local.namespace
   create_namespace = true
