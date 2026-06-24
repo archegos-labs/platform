648e87e786d4e88bac20995a8e650fa475746a99
Aeden Jameson <aeden.jameson@gmail.com>
2026-06-23 21:04:21 -0700

    version bump

--- kubeflow/terraform/main.tf
+++ kubeflow/terraform/main.tf
@@ -198,7 +198,7 @@ resource "helm_release" "kubeflow_trainer" {
 resource "helm_release" "kubeflow_pipelines" {
   name      = "kubeflow-pipelines"
   chart     = "../charts/pipelines"
-  version   = "1.1.1"
+  version   = "1.1.2"
   namespace = kubernetes_namespace.kubeflow.metadata[0].name
 
   timeout       = 600
